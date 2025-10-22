6de676e23244923ee3d51ef73e74588ead6dcfdb
Chermad <CHERMAD@GMAIL.com>
2025-10-22 11:43:49 +0100

    Test workflow Firebase

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Providers from "./components/Providers";
 
 export const metadata = {
-  title: "Mon site",
+  title: "Mon site next.js cree pour la premier fois",
 };
 
 export default function RootLayout({ children }) {
